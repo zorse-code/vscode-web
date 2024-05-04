0c21f9cc26baa1c4dbad826870b2628214503efb
Michael Neil <mneil@mneilsworld.com>
2024-05-03 19:46:39 -0700

    Fixed the build script to work again (#5)
    - Correct import of vscode workbench types
    - Install necessary dependencies in the workflow
    - Preemptive clean gyp to resolve errors recommended by vscode
    - Add information in the build script to debug where it fails

--- build.js
+++ build.js
@@ -6,43 +6,82 @@ const { version } = require("./package.json");
 
 const VSCODE_VERSION = version.split("-")[0];
 
-for (const tool of ["yarn", "git"]) {
+function error(msg) {
+  console.info("\x1b[31merror %s\x1b[0m", msg)
+}
+function ok(msg) {
+  console.info("\x1b[32m%s\x1b[0m", msg)
+}
+function note(msg) {
+  console.info("\x1b[90m%s\x1b[0m", msg)
+}
+function exec(cmd, opts) {
+  console.info("\x1b[36m%s\x1b[0m", cmd)
+  return child_process.execSync(cmd, opts);
+}
+
+const requiredTools = ["node", "yarn", "git", "python"];
+note(`required tools ${JSON.stringify(requiredTools)}`)
+for (const tool of requiredTools) {
   try {
     child_process.execSync(`${tool} --version`, { stdio: "ignore" });
   } catch (e) {
-    console.error(`"${tool}" is not available.`);
+    error(`"${tool}" is not available.`);
     process.exit(1);
   }
 }
+ok("required tools installed")
 
-if (!fs.existsSync("vscode")) {
-  child_process.execSync(`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${VSCODE_VERSION}`, {
-    stdio: "inherit",
-  });
-  child_process.execSync("cd vscode && git apply ../vscode.patch", { stdio: "inherit" });
+const node_version_out = child_process.execSync(`node -v`);
+const node_version = node_version_out.toString().trim()
+if (node_version < "v18.15" || node_version >= "v19.0") {
+  error(`Want node > 18.15 and < 19. Got "${node_version}"`);
+  process.exit(1);
 }
 
+if (!fs.existsSync("vscode")) {
+  note("cloning vscode")
+  exec(`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${VSCODE_VERSION}`, {
+    stdio: "inherit",
+  });
+  exec("cd vscode && git apply ../vscode.patch", { stdio: "inherit" });
+} else {
+  ok("vscodium already installed")
+  note("delete vscodium folder to clone again")
+}
+
+note("changing directory to vscode")
 process.chdir("vscode");
 
 if (!fs.existsSync("node_modules")) {
-  child_process.execSync("yarn", { stdio: "inherit" });
+  exec("yarn", { stdio: "inherit" });
+} else {
+  ok("node_modules exists. Skipping yarn")
 }
 
 // Use simple workbench
+note("copying workbench file")
 fs.copyFileSync("../workbench.ts", "src/vs/code/browser/workbench/workbench.ts");
 
 // Compile
-child_process.execSync("yarn gulp vscode-web-min", { stdio: "inherit" });
+note("starting compile")
+exec("yarn gulp vscode-web-min", { stdio: "inherit" });
+ok("compile completed")
 
 // Extract compiled files
 if (fs.existsSync("../dist")) {
+  note("cleaning ../dist")
   fs.rmSync("../dist", { recursive: true, force: true });
+} else {
+  ok("../dist did not exist. No need to clean")
 }
 
 fs.mkdirSync("../dist");
 fs.cpSync("../vscode-web", "../dist", { recursive: true, force: true });
+ok("copied ../vscode-web to ../dist")
 
 // Patch output
+note("beginning complex patches for networking")
 const distWorkbenchPath = "../dist/out/vs/workbench/workbench.web.main.js";
 const workbench = fs.readFileSync(distWorkbenchPath, "utf8");
 const workbenchPatched = workbench.replace(/("https:\/\/{{uuid}}[^"]+")/g, "(globalThis.VSCODE_WEB_PUBLIC_URL||$1)");
@@ -63,7 +102,7 @@ const patchOutput = async (destPath) => {
     const content = await fs.promises.readFile(destPath, "utf8");
     const stripped = content.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, "");
     if (stripped !== content) {
-      console.log(`Stripped sourcemaps from ${destPath}`);
+      note(`Stripped sourcemaps from ${destPath}`);
       await fs.promises.writeFile(destPath, stripped, "utf8");
       hasStripped = true;
     }
@@ -75,7 +114,7 @@ const patchOutput = async (destPath) => {
           re,
           `${_}if(${n}&&${n}.startsWith("://")){try{let o=globalThis.origin.split("://");${e}=o[0];${t}=o[1];${n}=${n}.replace("://","/")}catch(_){}};`
         );
-        console.log(`Patched ${destPath} for vscode-uri`);
+        ok(`Patched ${destPath} for vscode-uri`);
         await fs.promises.writeFile(destPath, patched, "utf8");
         hasPatched = true;
       }
